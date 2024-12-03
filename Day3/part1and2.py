2bfe009d1271f6e2951c7a02412078f38b7ec1ea
Cheese <64607897+cheese-1024@users.noreply.github.com>
2024-12-03 21:41:18 +0000

    made code shorter day3

--- Day3/part1and2.py
+++ Day3/part1and2.py
@@ -1,33 +1,22 @@
 import re
 
-total = 0
-uncorrupted_memory = []
-uncorrupted_memory2 = []
+uncorrupted_memory, sol1, sol2, do = [], 0, 0, True
 
 with open("input.txt", "r") as f:
   for line in f:
-    uncorrupted_memory += re.findall(r'mul\(\d{1,3},\d{1,3}\)', line.strip())
+    split = re.split(r"(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))", line.strip()) 
+    removeNone = [x for x in split if x is not None]
+    removeCorrupted = [x for x in removeNone if re.fullmatch(r'mul\(\d{1,3},\d{1,3}\)', x) is not None or re.fullmatch(r'don\'t\(\)', x) is not None or re.fullmatch(r'do\(\)', x) is not None]
+    uncorrupted_memory += removeCorrupted
 
 for instruction in uncorrupted_memory:
-  total += int(re.findall(r'(?<=mul\()\d{1,3}', instruction)[0]) * int(re.findall(r'mul\(\d{1,3},(\d{1,3})', instruction)[0])
-
-print(total)
-
-with open("input.txt", "r") as f:
-  for line in f:
-    temp = re.split(r"(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))", line.strip())
-    temp2 = [x for x in temp if x is not None]
-    temp3 = [x for x in temp2 if re.fullmatch(r'mul\(\d{1,3},\d{1,3}\)', x) is not None or re.fullmatch(r'don\'t\(\)', x) is not None or re.fullmatch(r'do\(\)', x) is not None]
-    uncorrupted_memory2 += temp3
-
-total, do = 0, True
-
-for instruction in uncorrupted_memory2:
+  if instruction != "do()" and instruction != "don't()":
+    sol1 += int(re.findall(r'(?<=mul\()\d{1,3}', instruction)[0]) * int(re.findall(r'mul\(\d{1,3},(\d{1,3})', instruction)[0])
   if instruction == "don't()":
     do = False
   elif instruction == "do()":
     do = True
   elif do:
-    total += int(re.findall(r'(?<=mul\()\d{1,3}', instruction)[0]) * int(re.findall(r'mul\(\d{1,3},(\d{1,3})', instruction)[0])
+    sol2 += int(re.findall(r'(?<=mul\()\d{1,3}', instruction)[0]) * int(re.findall(r'mul\(\d{1,3},(\d{1,3})', instruction)[0])
 
-print(total)
+print(sol1, sol2)
